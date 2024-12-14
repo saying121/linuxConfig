cbb4e27d6c0217252422df39b251d695086868dc
saying <saying121@outlook.co>
2024-12-14 14:39:03 +0800

    upate shells conf

--- shells/lib/common.sh
+++ shells/lib/common.sh
@@ -1,27 +1,32 @@
 # vim:fileencoding=utf-8:ft=sh
 # enable color support of ls, less and man, and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    # shellcheck disable=2015
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permissions
-
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias diff='diff --color=auto'
-    alias ip='ip -c=auto'
-
-    export LESS_TERMCAP_mb=$'\E[1;31m'  # begin blink
-    export LESS_TERMCAP_md=$'\E[1;36m'  # begin bold
-    export LESS_TERMCAP_me=$'\E[0m'     # reset bold/blink
-    export LESS_TERMCAP_so=$'\E[01;33m' # begin reverse video
-    export LESS_TERMCAP_se=$'\E[0m'     # reset reverse video
-    export LESS_TERMCAP_us=$'\E[1;32m'  # begin underline
-    export LESS_TERMCAP_ue=$'\E[0m'     # reset underline
+the_dircolors=dircolors
+# macos install `coreutils`
+if command -v gdircolors &> /dev/null; then
+    the_dircolors=gdircolors
 fi
+
+# shellcheck disable=2015
+test -r ~/.dircolors && eval "$($the_dircolors -b ~/.dircolors)" || eval "$($the_dircolors -b)"
+
+export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permissions
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip -c=auto'
+
+export LESS_TERMCAP_mb=$'\E[1;31m'  # begin blink
+export LESS_TERMCAP_md=$'\E[1;36m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'     # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'     # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'  # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'     # reset underline
 
 # alias paru="paru --skipreivew"
 alias kcat="kitty +kitten icat"
@@ -92,24 +97,24 @@ cat() {
 }
 
 # 确定发行版 kali㉿
-declare -A releaseDic
-releaseDic=(
-    [arch]=" "
-    [kali]=" "
-    [ubuntu]=" "
-    [suse]=" "
-    [manjaro]=" "
-    [pop]=" "
-    [deepin]=" "
-)
-index=$(awk -F= '/^ID/{print $2}' </etc/os-release)
-if [[ -n ${releaseDic[$index]} ]]; then
-    prompt_symbol=${releaseDic[$index]}
-else
-    # shellcheck disable=2034
-    prompt_symbol=" "
-fi
-unset releaseDic
+# declare -A releaseDic
+# releaseDic=(
+#     [arch]=" "
+#     [kali]=" "
+#     [ubuntu]=" "
+#     [suse]=" "
+#     [manjaro]=" "
+#     [pop]=" "
+#     [deepin]=" "
+# )
+# index=$(awk -F= '/^ID/{print $2}' </etc/os-release)
+# if [[ -n ${releaseDic[$index]} ]]; then
+#     prompt_symbol=${releaseDic[$index]}
+# else
+#     # shellcheck disable=2034
+#     prompt_symbol=" "
+# fi
+# unset releaseDic
 
 # wsl
 if [[ $(uname -a | grep -c WSL) != 0 ]]; then
@@ -202,8 +207,8 @@ debug_rust() {
     fi
 }
 
-thread_num=$(nproc)
-export MAKEFLAGS="-j$thread_num"
+# thread_num=$(nproc)
+export MAKEFLAGS="-j"
 
 export PATH=$PATH:~/.linuxConfig/scripts
 export PATH=$PATH:~/.ghcup/bin
--- shells/lib/zsh/omz.zsh
+++ shells/lib/zsh/omz.zsh
@@ -1,12 +1,11 @@
-# zi ice wait lucid; zi snippet OMZP::command-not-found
-source /usr/share/doc/find-the-command/ftc.zsh
+zi ice wait lucid; zi snippet OMZP::command-not-found
+# source /usr/share/doc/find-the-command/ftc.zsh
 
 zi ice wait lucid; zi snippet OMZP::extract
 zi ice wait lucid; zi snippet OMZP::thefuck
 
 zi ice wait lucid; zi snippet OMZL::clipboard.zsh
 zi ice wait lucid; zi snippet OMZL::functions.zsh
-zi ice wait lucid; zi snippet OMZL::history.zsh
 
 zi snippet OMZL::git.zsh
 zi ice wait lucid; zi snippet OMZP::git
