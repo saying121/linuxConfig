b11a66fdeb3b1933d04d531754ee590ffcb10971
saying <saying121@outlook.com>
2025-02-24 09:22:12 +0800

    tweak

--- nvim/lua/lsp_configs.lua
+++ nvim/lua/lsp_configs.lua
@@ -1,5 +1,10 @@
-local methods = vim.lsp.protocol.Methods
-local severity = vim.diagnostic.severity
+local vfn = vim.fn
+local api = vim.api
+local lsp = vim.lsp
+local util = lsp.util
+local diagnostic = vim.diagnostic
+local methods = lsp.protocol.Methods
+local severity = diagnostic.severity
 
 local signs = {
     [severity.ERROR] = "", --
@@ -18,8 +23,8 @@ local virtual_text = {
     severity = false,
     spacing = 4,
     -- severity = {
-    --     max = vim.diagnostic.severity.ERROR,
-    --     min = vim.diagnostic.severity.WARN,
+    --     max = diagnostic.severity.ERROR,
+    --     min = diagnostic.severity.WARN,
     -- },
 
     ---@param diagnostic vim.Diagnostic
@@ -46,7 +51,7 @@ local virtual_text = {
     end,
 }
 
-vim.diagnostic.config({
+diagnostic.config({
     virtual_text = false,
     -- virtual_text = virtual_text,
     float = { border = "single" },
@@ -63,44 +68,42 @@ vim.diagnostic.config({
 })
 
 -- 边框
-vim.lsp.handlers[methods.textDocument_hover] = vim.lsp.with(vim.lsp.handlers.hover, {
+lsp.handlers[methods.textDocument_hover] = lsp.with(lsp.handlers.hover, {
     border = "single",
 })
-vim.lsp.handlers[methods.textDocument_signatureHelp] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+lsp.handlers[methods.textDocument_signatureHelp] = lsp.with(lsp.handlers.signature_help, {
     border = "single",
 })
 
 local function goto_definition(split_cmd)
-  local util = vim.lsp.util
-  local log = require("vim.lsp.log")
-  local api = vim.api
+    local log = require("vim.lsp.log")
 
-  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-  local handler = function(_, result, ctx)
-    if result == nil or vim.tbl_isempty(result) then
-      local _ = log.info() and log.info(ctx.method, "No location found")
-      return nil
+    -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+    local handler = function(_, result, ctx)
+        if result == nil or vim.tbl_isempty(result) then
+            local _ = log.info() and log.info(ctx.method, "No location found")
+            return nil
+        end
+
+        if split_cmd then
+            vim.cmd(split_cmd)
+        end
+
+        if vim.islist(result) then
+            util.jump_to_location(result[1])
+
+            if #result > 1 then
+                -- util.set_qflist(util.locations_to_items(result))
+                vfn.setqflist(util.locations_to_items(result))
+                api.nvim_command("copen")
+                api.nvim_command("wincmd p")
+            end
+        else
+            util.jump_to_location(result)
+        end
     end
 
-    if split_cmd then
-      vim.cmd(split_cmd)
-    end
-
-    if vim.islist(result) then
-      util.jump_to_location(result[1])
-
-      if #result > 1 then
-        -- util.set_qflist(util.locations_to_items(result))
-        vim.fn.setqflist(util.locations_to_items(result))
-        api.nvim_command("copen")
-        api.nvim_command("wincmd p")
-      end
-    else
-      util.jump_to_location(result)
-    end
-  end
-
-  return handler
+    return handler
 end
 
-vim.lsp.handlers["textDocument/definition"] = goto_definition('vsplit')
+lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
---
+++ nvim/lua/plugins/lsps/nvim-metals.lua
@@ -0,0 +1,56 @@
+local api = vim.api
+
+---@type LazySpec
+return {
+    "scalameta/nvim-metals",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+        local metals_config = require("metals").bare_config()
+        ---@param client vim.lsp.Client
+        ---@param bufnr integer
+        metals_config.on_attach = function(client, bufnr)
+            require("public.lsp_attach").on_attach(client, bufnr)
+            -- require("metals").setup_dap()
+        end
+        metals_config.settings = {
+            useGlobalExecutable = false,
+            showImplicitArguments = false,
+            showImplicitConversionsAndClasses = false,
+            showInferredType = true,
+            superMethodLensesEnabled = false,
+            autoImportBuild = "initial",
+            serverProperties = {
+                "-Xmx2G",
+                "-Dmetals.enable-best-effort=true",
+                "-XX:+UseZGC",
+                "-XX:ZUncommitDelay=30",
+                "-XX:ZCollectionInterval=5",
+            },
+            inlayHints = {
+                hintsInPatternMatch = { enable = true },
+                implicitArguments = { enable = false },
+                implicitConversions = { enable = false },
+                inferredTypes = { enable = true },
+                typeParameters = { enable = false },
+            },
+        }
+        metals_config.init_options.statusBarProvider = "off"
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        metals_config.capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+
+        return metals_config
+    end,
+    config = function(self, metals_config)
+        local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
+        api.nvim_create_autocmd("FileType", {
+            pattern = self.ft,
+            callback = function()
+                require("metals").initialize_or_attach(metals_config)
+            end,
+            group = nvim_metals_group,
+        })
+    end,
+}
