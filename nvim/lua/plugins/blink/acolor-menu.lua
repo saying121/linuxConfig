8779bc1855497b67567112cdd1a2694d66bd7d48
saying <saying121@outlook.com>
2025-03-10 19:37:29 +0800

    neovim: update

--- nvim/lua/plugins/blink/acolor-menu.lua
+++ nvim/lua/plugins/blink/acolor-menu.lua
@@ -5,4 +5,12 @@ return {
     -- Shit, the plug need load befor blink.cmp, priority not work
     -- file name add "a" prefix
     priority = 2000,
+    opts = {
+        ls = {
+            ["rust-analyzer"] = {
+                -- Similar to the same setting of gopls.
+                align_type_to_right = false,
+            },
+        },
+    },
 }
