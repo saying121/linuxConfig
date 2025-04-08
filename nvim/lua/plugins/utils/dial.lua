0b7569a638522781da81ee9a932643d855daa715
saying121 <saying121@outlook.com>
2025-04-08 10:29:19 +0800

    neovim: update dial

--- nvim/lua/plugins/utils/dial.lua
+++ nvim/lua/plugins/utils/dial.lua
@@ -1,4 +1,6 @@
 local keymap = vim.keymap.set
+local api = vim.api
+
 ---@type LazySpec
 return {
     "monaqa/dial.nvim",
@@ -6,30 +8,49 @@ return {
         { "<C-a>", mode = { "n", "x" } },
         { "<C-x>", mode = { "n", "x" } },
     },
-    config = function()
+    init = function()
         local dial_map = require("dial.map")
+        local function mp(group)
+            keymap("n", "<C-a>", dial_map.inc_normal(group), { noremap = true })
+            keymap("n", "<C-x>", dial_map.dec_normal(group), { noremap = true })
+            keymap("n", "g<C-a>", dial_map.inc_gnormal(group), { noremap = true })
+            keymap("n", "g<C-x>", dial_map.dec_gnormal(group), { noremap = true })
+
+            keymap("x", "<C-a>", dial_map.inc_visual(group), { noremap = true })
+            keymap("x", "<C-x>", dial_map.dec_visual(group), { noremap = true })
+            keymap("x", "g<C-a>", dial_map.inc_gvisual(group), { noremap = true })
+            keymap("x", "g<C-x>", dial_map.dec_gvisual(group), { noremap = true })
+        end
+
+        api.nvim_create_autocmd({ "BufEnter" }, {
+            group = api.nvim_create_augroup("DialKeymap", { clear = true }),
+            pattern = { "*" },
+            callback = function()
+                if vim.bo.ft == "lua" then
+                    mp("lua")
+                else
+                    mp("default")
+                end
+            end,
+        })
+    end,
+    config = function()
         local augend = require("dial.augend")
+        local config = require("dial.config")
 
-        keymap("n", "<C-a>", dial_map.inc_normal("mygroup"), { noremap = true })
-        keymap("n", "<C-x>", dial_map.dec_normal("mygroup"), { noremap = true })
-        keymap("x", "<C-a>", dial_map.inc_visual("mygroup"), { noremap = true })
-        keymap("x", "<C-x>", dial_map.dec_visual("mygroup"), { noremap = true })
-        keymap("x", "g<C-a>", dial_map.inc_gvisual("mygroup"), { noremap = true })
-        keymap("x", "g<C-x>", dial_map.dec_gvisual("mygroup"), { noremap = true })
-
-        local mygroup = {
+        local default = {
             augend.integer.alias.decimal,
             augend.constant.alias.bool, -- boolean value (true <-> false)
             augend.constant.alias.alpha,
             augend.constant.alias.Alpha,
-            -- augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+            augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+            augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
             augend.integer.new({
                 radix = 16,
                 prefix = "0x",
                 natural = true,
                 case = "upper",
             }),
-            augend.date.alias["%m/%d/%Y"], -- date (02/19/2022, etc.)
             augend.constant.new({
                 elements = { "and", "or" },
                 word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
@@ -167,11 +188,6 @@ return {
                 case = "lower",
             }),
             augend.constant.new({
-                elements = { "!=", "==" },
-                word = false,
-                cyclic = true,
-            }),
-            augend.constant.new({
                 elements = { "left", "right" },
                 word = true,
                 cyclic = true,
@@ -197,35 +213,29 @@ return {
             }),
         }
 
-        -- if vim.bo.ft == "lua" then
-        --     table.insert(
-        --         mygroup,
-        --         augend.constant.new({
-        --             elements = { "==", "~=" },
-        --             word = true,
-        --             cyclic = true,
-        --         })
-        --     )
-        -- else
-        --     table.insert(
-        --         mygroup,
-        --         augend.constant.new({
-        --             elements = { "==", "!=" },
-        --             word = true,
-        --             cyclic = true,
-        --         })
-        --     )
-        -- end
+        local other = vim.deepcopy(default)
+        table.insert(
+            other,
+            augend.constant.new({
+                elements = { "==", "!=" },
+                word = false,
+                cyclic = true,
+            })
+        )
 
-        require("dial.config").augends:register_group({
-            -- default augends used when no group name is specified
-            default = {
-                augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
-                augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-                augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
-            },
-            -- augends used when group with name `mygroup` is specified
-            mygroup = mygroup,
+        local lua = default
+        table.insert(
+            lua,
+            augend.constant.new({
+                elements = { "==", "~=" },
+                word = false,
+                cyclic = true,
+            })
+        )
+
+        config.augends:register_group({
+            default = other,
+            lua = lua,
         })
     end,
 }
