802c0779e5a4fb5e321fea95dd1d12685048da09
saying <saying121@outlook.com>
2025-05-07 20:25:06 +0800

    neovim move config to lsp_config

--- nvim/lua/lsp_configs.lua
+++ nvim/lua/lsp_configs.lua
@@ -108,3 +108,21 @@ lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
 lsp.config("*", {
     on_attach = lsp_attach.on_attach,
 })
+
+-- 要禁用某个 lsp 就去改后缀名
+local lsp_path = vfn.stdpath("config") .. "/after/lsp"
+local file_name_list = vfn.readdir(lsp_path)
+
+local lsps = {}
+for _, the_file_name in ipairs(file_name_list) do
+    if not vim.endswith(the_file_name, ".lua") then
+        goto continue
+    end
+    local lsp_name = string.sub(the_file_name, 1, #the_file_name - 4)
+
+    table.insert(lsps, lsp_name)
+
+    ::continue::
+end
+
+lsp.enable(lsps)
--- nvim/lua/plugins/lsps/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsps/nvim-lspconfig.lua
@@ -1,36 +1,5 @@
-local vfn = vim.fn
-
 ---@type LazySpec
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre" },
-    config = function()
-        local lspconfig = require("lspconfig")
-
-        require("lspconfig.ui.windows").default_options.border = "single"
-
-        lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-            autostart = true,
-        })
-
-        local lsp = vim.lsp
-
-        -- 要禁用某个 lsp 就去改后缀名
-        local lsp_path = vfn.stdpath("config") .. "/after/lsp"
-        local file_name_list = vfn.readdir(lsp_path)
-
-        local lsps = {}
-        for _, the_file_name in ipairs(file_name_list) do
-            if not vim.endswith(the_file_name, ".lua") then
-                goto continue
-            end
-            local lsp_name = string.sub(the_file_name, 1, #the_file_name - 4)
-
-            table.insert(lsps, lsp_name)
-
-            ::continue::
-        end
-
-        lsp.enable(lsps)
-    end,
 }
