41f70642b1f5d4561974991ce587b88e2ebbc41f
saying <saying121@outlook.com>
2025-05-22 19:47:29 +0800

    update

--- nvim/lua/lsp_configs.lua
+++ nvim/lua/lsp_configs.lua
@@ -9,8 +9,6 @@ local log = vim.lsp.log
 
 local lsp_attach = require("public.lsp_attach")
 
-local lsp_attach = require("public.lsp_attach")
-
 local signs = {
     [severity.ERROR] = "", --
     [severity.WARN] = "", --
