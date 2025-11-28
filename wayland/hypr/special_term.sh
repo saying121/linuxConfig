2983e0b533003db74d585184974e48e4fcfc1d08
saying121 <saying121@outlook.com>
2025-11-28 14:20:11 +0800

    update hypr

--- wayland/hypr/special_term.sh
+++ wayland/hypr/special_term.sh
@@ -6,6 +6,5 @@ name=term
 if [[ $(hyprctl workspaces | grep -c special:"$name") == 1 ]]; then
     hyprctl dispatch togglespecialworkspace $name
 else
-    hyprctl dispatch exec \[workspace special:$name \;float\;size 70% 70%\;center\] kitty #&&
-    # hyprctl dispatch togglespecialworkspace $name
+    hyprctl dispatch exec \[workspace special:$name \;float\;size monitor_w*0.7 monitor_h*0.7\;center\] kitty #&&
 fi
